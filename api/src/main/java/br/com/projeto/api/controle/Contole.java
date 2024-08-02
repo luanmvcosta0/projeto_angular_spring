a7da9beaea962751a2796a3f81fd80c50692603a
luanmvcosta0 <luanmvcosta@mgail.com>
2024-08-01 22:29:42 -0300

    Adicionando rota de cadastro de usuario

--- api/src/main/java/br/com/projeto/api/controle/Contole.java
+++ api/src/main/java/br/com/projeto/api/controle/Contole.java
@@ -1,10 +1,24 @@
 package br.com.projeto.api.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.modelo.Cliente;
+import br.com.projeto.api.repositorio.Repositorio;
 
 @RestController
 public class Contole {
+
+    @Autowired
+    private Repositorio acao;
+
+    @PostMapping("/")
+    public Cliente cadastrar(@RequestBody Cliente c) {
+        return acao.save(c);
+    }
 
     @GetMapping("/teste")
     public String teste() {
